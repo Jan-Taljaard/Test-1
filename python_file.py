b0121f133f1fdec8d0172de8c857b72c6649f9f0
Jan-Taljaard Marx <jantaljaardmarx@gmail.com>
2021-11-22 12:05:50 +0200

    Python file upload test

---
+++ python_file.py
@@ -0,0 +1,6 @@
+print("Welcome to the boday mass index calculator")
+height = input("Please enter your height in m: ")
+weight = input("Please enter your weight in kg: ")
+a = float(height)
+b = float(weight)
+print(round(b / a**2))
